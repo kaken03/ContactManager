c30de7189da212c400d24771e62ec2a7d506e443
kaken03 <kentezekielvillahermosa@gmail.com>
2026-02-15 12:16:14 +0800

    Fix admin UI - add WhiteNoise middleware for static files and collectstatic to release phase

--- ContactList/settings.py
+++ ContactList/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Serve static files
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
