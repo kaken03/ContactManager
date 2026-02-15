cafc3ece119bdf996d347189f96536556e1022e5
kaken03 <kentezekielvillahermosa@gmail.com>
2026-02-15 11:52:12 +0800

    Fix: remove invalid atomic_requests parameter from dj_database_url.config()

--- ContactList/settings.py
+++ ContactList/settings.py
@@ -92,7 +92,6 @@ if os.getenv('DATABASE_URL'):
             default=os.getenv("DATABASE_URL"),
             conn_max_age=600,
             conn_health_checks=True,
-            atomic_requests=False,
         )
     }
 else:
